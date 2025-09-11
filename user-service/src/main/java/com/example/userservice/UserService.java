30bdf1e7ac3102a713cecf9bd9bdd8cc8b02129b
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-09-11 16:58:37 +0900

    회원가입 API, 비즈니스 로직

--- user-service/src/main/java/com/example/userservice/UserService.java
+++ user-service/src/main/java/com/example/userservice/UserService.java
@@ -14,6 +14,30 @@ public class UserService {
     }
 
     public void signUp(SignUpRequestDto signUpRequestDto) {
-        
+        // 회원 가입한 사용자 정보 DB에 저장
+        User user = new User(
+                signUpRequestDto.getEmail(),
+                signUpRequestDto.getName(),
+                signUpRequestDto.getPassword()
+        );
+        User savedUser = userRepository.save(user);
+
+        // 카프카에 메시지 전송
+        UserSignedUpEvent userSignedUpEvent = new UserSignedUpEvent(
+                savedUser.getId(),
+                savedUser.getEmail(),
+                savedUser.getName()
+        );
+        this.kafkaTemplate.send("user.signed-up", toJsonString(userSignedUpEvent));
+    }
+
+    private String toJsonString(Object object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String message = objectMapper.writeValueAsString(object);
+            return message;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Json 직렬화 실패");
+        }
     }
 }
