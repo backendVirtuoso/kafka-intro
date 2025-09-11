7804f64b5351f3e47bfb232206de7bf27ae45d23
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-09-11 17:32:34 +0900

    yml, import 수정

--- user-service/src/main/java/com/example/userservice/UserService.java
+++ user-service/src/main/java/com/example/userservice/UserService.java
@@ -1,5 +1,7 @@
 package com.example.userservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
